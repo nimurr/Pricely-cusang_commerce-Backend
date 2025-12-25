1a8e2c85bc4aa7ce0f5236dad52614a3b33cdc34
nimurr <nimurnerob404@gmail.com>
2025-12-25 10:28:13 +0600

    first commit

---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports.authController = require('./auth.controller');
+module.exports.userController = require('./user.controller');
+module.exports.analysisHistoryController = require('./analysisHistoryRoute.controller');
+
+
+
---
+++ src/routes/v1/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const config = require("../../config/config");
+const authRoute = require("./auth.routes");
+const userRoute = require("./user.routes");
+const docsRoute = require("./docs.routes");
+const analysisHistoryRoute = require("./analysisHistoryRoute.routes");
+const webhookRoute = require("./webhook.routes");
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: "/auth",
+    route: authRoute,
+  },
+  {
+    path: "/users",
+    route: userRoute,
+  },
+  {
+    path: "/analysis-history",
+    route: analysisHistoryRoute,
+  },
+  {
+    path: "/checkout",
+    route: webhookRoute,
+  }
+
+];
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: "/docs",
+    route: docsRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === "development") {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
